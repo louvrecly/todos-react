4d9f656ddbc242d876cbaff21625ad576e0d38db
Louvre Chow <louvrecly@gmail.com>
2021-05-19 21:22:47 +0800

    feat/json-placeholder-api: Create userId and todos states and initTodos method in App and call it in useEffect to retrieve and display todos in the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { fetchTodos } from './services/jsonPlaceholderApi';
+import { Todo } from './constants/types';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [userId, setUserId] = useState<number>(1); // eslint-disable-line
+  const [todos, setTodos] = useState<Array<Todo>>([]);
+
+  async function initTodos(userId: number) {
+    const { data } = await fetchTodos(userId);
+    setTodos(data as Array<Todo>);
+  }
+
+  useEffect(() => {
+    initTodos(userId);
+  }, [userId]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todos</h1>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{JSON.stringify(todo)}</li>
+        ))}
+      </ul>
     </div>
   );
 }
