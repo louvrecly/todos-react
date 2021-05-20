6fc88014d1e1f034531ec1b6e0890660dffe1980
Louvre Chow <louvrecly@gmail.com>
2021-05-20 23:49:51 +0800

    feat/redux-store: Create checkSuccessfulStatus util and Create postTodo, putTodo and deleteTodo in jsonPlaceholderApi service

--- src/services/jsonPlaceholderApi.ts
+++ src/services/jsonPlaceholderApi.ts
@@ -1,12 +1,25 @@
 import axios from 'axios';
+import { Todo } from '../constants/types';
 
 const jsonPlaceholderClient = axios.create({
   baseURL: process.env.REACT_APP_JSON_PLACEHOLDER_API
 });
 
 /* Todos endpoints */
-export function fetchTodos(userId = 1) {
+export function fetchTodos(userId: number = 1) {
   return jsonPlaceholderClient.get('/todos', {
     params: { userId }
   });
+}
+
+export function postTodo(newTodo: Todo) {
+  return jsonPlaceholderClient.post('/todos', newTodo);
+}
+
+export function putTodo(updatedTodo: Todo) {
+  return jsonPlaceholderClient.put(`/todos/${updatedTodo.id as number}`, updatedTodo);
+}
+
+export function deleteTodo(id: number) {
+  return jsonPlaceholderClient.delete(`/todos/${id}`);
 }
