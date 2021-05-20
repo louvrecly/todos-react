70b5146787aa25eadc14edcb6062177eca37cf5e
Louvre Chow <louvrecly@gmail.com>
2021-05-20 21:01:16 +0800

    feat/redux-store: Replace local todos state with todos state from redux store in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import TodoList from './components/TodoList';
 import { fetchTodos } from './services/jsonPlaceholderApi';
 import { Todo } from './constants/types';
+import { setTodos } from './redux/todos/actions';
 import classes from './App.module.scss';
+import { IRootState } from './redux/store';
 
 const App: React.FC = () => {
   const [userId, setUserId] = useState<number>(1); // eslint-disable-line
-  const [todos, setTodos] = useState<Array<Todo>>([]);
+  const todos = useSelector((state: IRootState) => state.todos.todos);
+  const dispatch = useDispatch();
 
   async function initTodos(userId: number) {
     const { data } = await fetchTodos(userId);
-    setTodos(data as Array<Todo>);
+    dispatch(setTodos(data as Array<Todo>));
   }
 
   useEffect(() => {
     initTodos(userId);
-  }, [userId]);
+  }, [userId]); // eslint-disable-line
 
   return (
     <div className={classes.app}>
