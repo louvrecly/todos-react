051a4022fad5b24e4377c0106d0ddd2b66e6338f
Louvre Chow <louvrecly@gmail.com>
2021-05-20 21:02:03 +0800

    feat/redux-store: Add updateTodo and removeTodo actions in TodoList

--- src/components/TodoList/TodoItem/index.tsx
+++ src/components/TodoList/TodoItem/index.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import clsx from 'clsx';
 import { Todo } from '../../../constants/types';
+import { removeTodo, updateTodo } from '../../../redux/todos/actions';
 import classes from './style.module.scss';
 
 interface ITodoItemProps {
   todo: Todo
 }
 
+type FieldKeyType = 'completed' | 'title';
+type TargetKeyType = 'checked' | 'value';
+
 const TodoItem: React.FC<ITodoItemProps> = (props: ITodoItemProps) => {
+  const dispatch = useDispatch();
+
+  const eventTargetFieldGetter = {
+    completed: 'checked',
+    title: 'value'
+  }
+
+  const handleInput = (key: FieldKeyType): React.ChangeEventHandler<HTMLInputElement> => (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedTodo = {
+      ...props.todo,
+      [key]: e.target[eventTargetFieldGetter[key] as TargetKeyType]
+    };
+    dispatch(updateTodo(updatedTodo));
+  };
+
+  const handleOnClick = (id: number): React.MouseEventHandler<HTMLButtonElement> => () => {
+    dispatch(removeTodo(id));
+  };
+
   return (
     <li className={classes['todo-item']}>
       <div className={classes.container}>
@@ -15,17 +39,17 @@ const TodoItem: React.FC<ITodoItemProps> = (props: ITodoItemProps) => {
           classes.checker,
           props.todo.completed && classes.checked
         ])}>
-          <input type="checkbox" className={classes.checkbox} checked={props.todo.completed} />
+          <input type="checkbox" className={classes.checkbox} checked={props.todo.completed} onChange={handleInput('completed')} />
         </div>
 
-        <input type="text" className={classes.title} value={props.todo.title} />
+        <input type="text" className={classes.title} value={props.todo.title} onChange={handleInput('title')} />
 
-        <button className={classes.button}>
+        <button className={classes.button} onClick={handleOnClick(props.todo.id as number)}>
           <span className={classes.icon}>×</span>
         </button>
       </div>
     </li>
   );
-}
+};
 
 export default TodoItem;
