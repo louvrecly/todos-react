c1821e029bdc07823f7f69c9e0beb679ca4e9228
Louvre Chow <louvrecly@gmail.com>
2021-05-21 14:23:25 +0800

    enh/user-dropdown: Add select input for handling change in userId in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import NewTodo from './components/NewTodo';
 import TodoList from './components/TodoList';
 import { loadTodos } from './redux/todos/thunks';
 import { IRootState } from './redux/store';
+import { userIds } from './constant';
 import classes from './App.module.scss';
 
 const App: React.FC = () => {
@@ -15,13 +16,25 @@ const App: React.FC = () => {
     dispatch(loadTodos(userId));
   }
 
+  const handleSelect: React.ChangeEventHandler<HTMLSelectElement> = (e: ChangeEvent<HTMLSelectElement>) => {
+    setUserId(parseInt(e.target.value));
+  };
+
   useEffect(() => {
     initTodos(userId);
   }, [userId]); // eslint-disable-line
 
   return (
     <div className={classes.app}>
-      <h1 className={classes.title}>User {userId}'s Todo List</h1>
+      <div className={classes.header}>
+        <h1 className={classes.title}>User {userId}'s Todo List</h1>
+
+        <select className={classes.select} value={userId} onChange={handleSelect}>
+          {userIds.map(id => (
+            <option key={id} className={classes.option} value={id}>User {id}</option>
+          ))}
+        </select>
+      </div>
 
       <NewTodo userId={userId} />
 
