ab6a16e8fa4cfbbcf3e044b1e0d229039144715e
Louvre Chow <louvrecly@gmail.com>
2021-05-21 10:39:29 +0800

    feat/filter-control: Add filter control to TodoList using displayTypes constant and displayType local state

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clsx from 'clsx';
 import TodoItem from './TodoItem';
 import { Todo } from '../../constants/types';
+import { DisplayType, displayTypes } from './constant';
 import classes from './style.module.scss';
 
 interface ITodoListProps {
@@ -8,12 +10,34 @@ interface ITodoListProps {
 }
 
 const TodoList: React.FC<ITodoListProps> = (props: ITodoListProps) => {
+  const [displayType, setDisplayType] = useState<DisplayType>('active');
+
   return (
-    <ul className={classes['todo-list']}>
-      {props.todos.map(todo => (
-        <TodoItem key={todo.id} todo={todo} />
-      ))}
-    </ul>
+    <div className={classes['todo-list']}>
+      <ul className={classes.tiles}>
+        {(Object.keys(displayTypes) as Array<DisplayType>)
+          .map(key => (
+            <li
+              key={key}
+              className={clsx([
+                classes.tile,
+                key === displayType && classes[key]
+              ])}
+              onClick={() => setDisplayType(key)}
+            >{displayTypes[key].title}</li>
+          ))
+        }
+      </ul>
+
+      <ul className={classes['todos']}>
+        {props.todos
+          .filter(displayTypes[displayType].filter)
+          .map(todo => (
+            <TodoItem key={todo.id} todo={todo} />
+          ))
+        }
+      </ul>
+    </div>
   );
 }
 
