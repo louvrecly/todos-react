a0a48af0b2be2642d4217b6ce10cfe54b9cc4d63
Louvre Chow <louvrecly@gmail.com>
2021-05-21 17:14:56 +0800

    enh/theme: Create IStylesState, IStylesAction and stylesReducer and Import them into redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,23 @@
 import { combineReducers, Store, createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { ITodosState } from "./todos/state";
+import { IStylesState } from './styles/state';
 import { ITodosAction } from "./todos/actions";
+import { IStylesAction } from './styles/actions';
 import { todosReducer } from './todos/reducer';
+import { stylesReducer } from './styles/reducer';
 
 export interface IRootState {
-  todos: ITodosState
+  todos: ITodosState,
+  styles: IStylesState
 }
 
-type IRootAction = ITodosAction;
+type IRootAction = ITodosAction | IStylesAction;
 
-const rootReducer = combineReducers<IRootState, IRootAction>({ todos: todosReducer });
+const rootReducer = combineReducers<IRootState, IRootAction>({
+  todos: todosReducer as any,
+  styles: stylesReducer as any
+});
 
 const store: Store<IRootState, IRootAction> = createStore(
   rootReducer,
