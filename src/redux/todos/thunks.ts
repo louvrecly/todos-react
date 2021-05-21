e208a186f330306173b63a39269ded2b5f77deeb
Louvre Chow <louvrecly@gmail.com>
2021-05-21 10:43:04 +0800

    feat/filter-control: Comment out putTodo and deleteTodo in thunks to enhance app performance

--- src/redux/todos/thunks.ts
+++ src/redux/todos/thunks.ts
@@ -1,7 +1,7 @@
 import { Dispatch } from "redux";
 import { Todo } from "../../constants/types";
 import { createTodoSuccess, failed, ITodosAction, loadTodosSuccess, removeTodoSuccess, updateTodoSuccess } from "./actions";
-import { deleteTodo, fetchTodos, postTodo, putTodo } from "../../services/jsonPlaceholderApi";
+import { fetchTodos, postTodo } from "../../services/jsonPlaceholderApi";
 import checkSuccessfulStatus from "../../utils/checkSuccessfulStatus";
 
 export function loadTodos(userId: number) {
@@ -23,19 +23,23 @@ export function createTodo(newTodo: Todo) {
 }
 
 export function updateTodo(updatedTodo: Todo) {
-  return async (dispatch: Dispatch<ITodosAction>) => {
-    const { status, statusText, data } = await putTodo(updatedTodo);
+  return (dispatch: Dispatch<ITodosAction>) => dispatch(updateTodoSuccess(updatedTodo));
+  /* Working code - commented out for performance concern since the Json Placeholder API are faked */
+  // return async (dispatch: Dispatch<ITodosAction>) => {
+  //   const { status, statusText, data } = await putTodo(updatedTodo);
 
-    if (checkSuccessfulStatus(status)) dispatch(updateTodoSuccess(data as Todo));
-    else dispatch(failed('UPDATE_TODO_FAILED', statusText));
-  };
+  //   if (checkSuccessfulStatus(status)) dispatch(updateTodoSuccess(data as Todo));
+  //   else dispatch(failed('UPDATE_TODO_FAILED', statusText));
+  // };
 }
 
 export function removeTodo(id: number) {
-  return async (dispatch: Dispatch<ITodosAction>) => {
-    const { status, statusText } = await deleteTodo(id);
+  return (dispatch: Dispatch<ITodosAction>) => dispatch(removeTodoSuccess(id));
+  /* Working code - commented out for performance concern since the Json Placeholder API are faked */
+  // return async (dispatch: Dispatch<ITodosAction>) => {
+  //   const { status, statusText } = await deleteTodo(id);
 
-    if (checkSuccessfulStatus(status)) dispatch(removeTodoSuccess(id));
-    else dispatch(failed('REMOVE_TODO_FAILED', statusText));
-  };
+  //   if (checkSuccessfulStatus(status)) dispatch(removeTodoSuccess(id));
+  //   else dispatch(failed('REMOVE_TODO_FAILED', statusText));
+  // };
 }
