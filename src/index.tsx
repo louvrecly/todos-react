4813fbe56a744f031546b2494a69c0b8377e30b8
Louvre Chow <louvrecly@gmail.com>
2021-08-04 18:13:27 +0800

    feat/react-router-dom: Wrap App with ConnectedRouter with history created from store

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
 import App from './App';
-import store from './redux/store';
+import store, { history } from './redux/store';
 import env from './config/dotenv'; // eslint-disable-line
 import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
